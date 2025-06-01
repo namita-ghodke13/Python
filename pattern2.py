4f15ed939394ee5c23f07a9897930bbfd630ad47
Namita Ghodke <138099710+namita-ghodke13@users.noreply.github.com>
2025-06-01 23:47:21 +0530

    Add files via upload

---
+++ pattern2.py
@@ -0,0 +1,3 @@
+for i in range(0,3,1):
+   print(" "* (i-1),end=" ")
+   print("   * "*i)
---
+++ prime.py
@@ -0,0 +1,6 @@
+for n in range(100,200):
+   for i in range(2,n):
+      if n%i==0:
+         break;
+   if i==n-1:
+      print(n,end=" ")
